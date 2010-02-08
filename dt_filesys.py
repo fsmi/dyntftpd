b91009707125911cdefb5de0d6c0555ed6baa0ea
Fabian Knittel <fabian.knittel@avona.com>
2010-02-08 02:27:32 +0100

    dt_filesys.py: update to revised tftpy filesys interface.

--- dt_filesys.py
+++ dt_filesys.py
@@ -47,39 +47,9 @@ class FileSystemStack(object):
 					path, sub_path))
 			if sub_path is not None:
 				fs_path = file_sys.get_path(sub_path)
-				if fs_path.exists():
+				if fs_path is not None:
 					return fs_path
-		return UnknownFilePath(path)
-
-class UnknownFilePath(object):
-	"""UnknownFilePath is used as a fall-back path object, in case the
-	FileSystemStack found no file system that feels responsible for the path."""
-
-	def __init__(self, path):
-		self.path = path
-
-	def safe(self):
-		"""There is no direct file-system access, so we're always sure that
-		the path is safe. We'll catch any odd cases through the pattern
-		matching."""
-		return True
-
-	def exists(self):
-		return False
-
-	def open_read(self):
-		raise RuntimeError("Attempted to open UnknownFilePath(%s)" % self.path)
-
-	def get_path(self):
-		return self.path
-
-	def get_size(self):
-		raise RuntimeError("Attempted to get size of UnknownFilePath(%s)" % (
-				self.path))
-
-	def __str__(self):
-		return self.path
-
+		return None
 
 class SanitiseRequestFileSystemFilter(object):
 	def __init__(self, fs):
@@ -99,44 +69,31 @@ class SimulatedFileSystem(object):
 		self.handlers.append((regex, handler_func))
 
 	def get_path(self, path):
-		return SimulatedFilePath(self, path)
-
-class SimulatedFilePath(object):
-	def __init__(self, file_sys, path):
-		self.file_sys = file_sys
-
 		if len(path) > 0 and path[0] != '/':
 			path = '/' + path
-		self.path = os.path.normpath(path)
+		path = os.path.normpath(path)
 
 		# Find matching handler and execute it.
-		self.fileobj = None
-		self.size = 0
-		for regex, handler in self.file_sys.handlers:
+		for regex, handler in self.handlers:
 			logger.debug('Matching %s against %s' % (regex, path))
 			match = re.search(regex, path)
 			if match is not None:
-				fileobj, size = handler(self.path, match)
+				fileobj, size = handler(path, match)
 				if fileobj is not None:
-					self.fileobj = fileobj
-					self.size = size
+					return SimulatedFilePath(path, fileobj, size)
 					break
 				else:
-					logger.info("Matching handler failed to produce a file name.")
+					logger.info("Path '%s' matched handler's regex '%s', but " \
+							"handler came back empty." % (path, regex))
+		return None
 
-	def safe(self):
-		"""There is no direct file-system access, so we're always sure that
-		the path is safe. We'll catch any odd cases through the pattern
-		matching."""
-		return True
-
-	def exists(self):
-		return self.fileobj is not None
+class SimulatedFilePath(object):
+	def __init__(self, path, fileobj, size):
+		self.path = path
+		self.fileobj = fileobj
+		self.size = size
 
 	def open_read(self):
-		if self.fileobj is None:
-			raise RuntimeError("Attempted to open simulated path that " \
-					"does not exist")
 		return self.fileobj
 
 	def get_path(self):
