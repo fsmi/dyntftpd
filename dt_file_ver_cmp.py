d1da34a2186db9ad370fca49b67e68458e895e60
Fabian Knittel <fabian.knittel@fsmi.uni-karlsruhe.de>
2010-03-17 12:22:55 +0100

    Switch indentation from 4-space tabs to 4-space spaces.

--- dt_file_ver_cmp.py
+++ dt_file_ver_cmp.py
@@ -20,86 +20,86 @@ import os
 import re
 
 def split_ver(ver):
-	return ver.split('-', 1)
+    return ver.split('-', 1)
 
 def split_file_ver(base, suffix, name):
-	"""Split off base and suffix from the file-name and return the file's
-	version."""
-	return name[len(base):-len(suffix)]
+    """Split off base and suffix from the file-name and return the file's
+    version."""
+    return name[len(base):-len(suffix)]
 
 def cmp_maybe_int(val1, val2):
-	"""Compare two string values. In case they're numeric, compare the
-	numeric values, otherwise do a alphanumeric compare. In case only one of
-	them is numeric, the numeric value wins.
-	"""
-	try:
-		int_val1 = int(val1)
-	except:
-		int_val1 = None
-	try:
-		int_val2 = int(val2)
-	except:
-		int_val2 = None
+    """Compare two string values. In case they're numeric, compare the
+    numeric values, otherwise do a alphanumeric compare. In case only one of
+    them is numeric, the numeric value wins.
+    """
+    try:
+        int_val1 = int(val1)
+    except:
+        int_val1 = None
+    try:
+        int_val2 = int(val2)
+    except:
+        int_val2 = None
 
-	if int_val1 is not None or int_val2 is not None:
-		return cmp(int_val1, int_val2)
-	return cmp(val1, val2)
+    if int_val1 is not None or int_val2 is not None:
+        return cmp(int_val1, int_val2)
+    return cmp(val1, val2)
 
 def cmp_dotted_ver(ver1, ver2):
-	# Trivial case.
-	if ver1 == ver2:
-		return 0
+    # Trivial case.
+    if ver1 == ver2:
+        return 0
 
-	# Compare each component.
-	vers1 = ver1.split('.')
-	vers2 = ver2.split('.')
-	i = 0
-	while i < len(vers1) and i < len(vers2):
-		if vers1[i] != vers2[i]:
-			return cmp_maybe_int(vers1[i], vers2[i])
-		i += 1
+    # Compare each component.
+    vers1 = ver1.split('.')
+    vers2 = ver2.split('.')
+    i = 0
+    while i < len(vers1) and i < len(vers2):
+        if vers1[i] != vers2[i]:
+            return cmp_maybe_int(vers1[i], vers2[i])
+        i += 1
 
-	# The longer version with otherwise equal components is the newer / higher
-	# version.
-	if len(vers1) > len(vers2):
-		return 1
-	else:
-		return -1
+    # The longer version with otherwise equal components is the newer / higher
+    # version.
+    if len(vers1) > len(vers2):
+        return 1
+    else:
+        return -1
 
 def cmp_ver(ver1, ver2):
-	# Trivial case.
-	if ver1 == ver2:
-		return 0
+    # Trivial case.
+    if ver1 == ver2:
+        return 0
 
-	front1, back1 = split_ver(ver1)
-	front2, back2 = split_ver(ver2)
-	# If the front version is equal, compare the appended version.
-	if front1 == front2:
-		return cmp_maybe_int(back1, back2)
+    front1, back1 = split_ver(ver1)
+    front2, back2 = split_ver(ver2)
+    # If the front version is equal, compare the appended version.
+    if front1 == front2:
+        return cmp_maybe_int(back1, back2)
 
-	return cmp_dotted_ver(front1, front2)
+    return cmp_dotted_ver(front1, front2)
 
 def cmp_files(base, suffix, name1, name2):
-	if name1 == name2:
-		return 0
-	ver1 = split_file_ver(base, suffix, name1)
-	ver2 = split_file_ver(base, suffix, name2)
-	return cmp_ver(ver1, ver2)
+    if name1 == name2:
+        return 0
+    ver1 = split_file_ver(base, suffix, name1)
+    ver2 = split_file_ver(base, suffix, name2)
+    return cmp_ver(ver1, ver2)
 
 def highest_file_ver(base, suffix, files):
-	highest_file = files[0]
-	for file in files[1:]:
-		if cmp_files(base, suffix, file, highest_file) == 1:
-			highest_file = file
-	return highest_file
+    highest_file = files[0]
+    for file in files[1:]:
+        if cmp_files(base, suffix, file, highest_file) == 1:
+            highest_file = file
+    return highest_file
 
 def find_highest_file_ver(base, suffix, search_dir):
-	regex = '^%s.+%s$' % (base, suffix)
-	prog = re.compile(regex)
-	files = filter(prog.search, os.listdir(search_dir))
-	if len(files) > 0:
-		return os.path.join(search_dir, highest_file_ver(base, suffix, files))
-	else:
-		return None
+    regex = '^%s.+%s$' % (base, suffix)
+    prog = re.compile(regex)
+    files = filter(prog.search, os.listdir(search_dir))
+    if len(files) > 0:
+        return os.path.join(search_dir, highest_file_ver(base, suffix, files))
+    else:
+        return None
 
-# vim:set ft=python ts=4 noet:
+# vim:set ft=python sw=4 et:
--- examples/boot_cfg.py
+++ examples/boot_cfg.py
@@ -1,31 +1,33 @@
 import boot_label
 
 labels.append(LocalBootLabel(name = 'hdd', desc = 'Von ^Festplatte starten',
-		help = 'Beendet den Netzwerkbootprozess und startet ' \
-				'normal von der Festplatte.',
-		default = True))
+        help = 'Beendet den Netzwerkbootprozess und startet ' \
+                'normal von der Festplatte.',
+        default = True))
 
 labels.append(ChainBootLabel(name = 'winxp', hd = 'hd0', part = 3,
-		desc = '^Windows XP Professional',
-		help = 'Startet Windows XP von der Festplatte.'))
+        desc = '^Windows XP Professional',
+        help = 'Startet Windows XP von der Festplatte.'))
 labels.append(LinuxNfsRootBootLabel(name = 'linux', kernel = 'vmlinuz-i686',
-		initrd = 'initrd.img-i686',
-		nfsroot = 'nfsroot=10.14.1.3:/srv/nfs/clients/test,' \
-				'v3,tcp,rsize=32768,wsize=32768',
-		append = 'quiet vga=normal ramdisk_size=14332 ' \
-				'ro init=/usr/local/sbin/startup DISK=/dev/hda1',
-		desc = '^FSMI Linux (Kernel: aktuell / Image: test)'))
+        initrd = 'initrd.img-i686',
+        nfsroot = 'nfsroot=10.14.1.3:/srv/nfs/clients/test,' \
+                'v3,tcp,rsize=32768,wsize=32768',
+        append = 'quiet vga=normal ramdisk_size=14332 ' \
+                'ro init=/usr/local/sbin/startup DISK=/dev/hda1',
+        desc = '^FSMI Linux (Kernel: aktuell / Image: test)'))
 
 labels.append(SeparatorBootLabel(desc = 'Admin:'))
 labels.append(LinuxBootLabel(name = 'rescue', kernel = 'd-i/linux-etch-i386',
-		initrd = 'd-i/initrd-etch-i386',
-		append = 'vga=normal rescue/enable=true --',
-		desc = '^Wiederherstellungskonsole',
-		help = 'Startet einen Debian-Installer (etch/i386) im ' \
-				'Rescue Mode.',
-		password = '<placeholder>', indent = 1))
+        initrd = 'd-i/initrd-etch-i386',
+        append = 'vga=normal rescue/enable=true --',
+        desc = '^Wiederherstellungskonsole',
+        help = 'Startet einen Debian-Installer (etch/i386) im ' \
+                'Rescue Mode.',
+        password = '<placeholder>', indent = 1))
 labels.append(LinuxBootLabel(name = 'install', kernel = 'd-i/linux-etch-i386',
-		initrd = 'd-i/initrd-etch-i386', append = 'vga=normal --',
-		desc = '^Debian-Installer (etch/i386)',
-		help = 'Startet einen Debian-Installer (etch/i386).',
-		password = '<placeholder>', indent = 1))
+        initrd = 'd-i/initrd-etch-i386', append = 'vga=normal --',
+        desc = '^Debian-Installer (etch/i386)',
+        help = 'Startet einen Debian-Installer (etch/i386).',
+        password = '<placeholder>', indent = 1))
+
+# vim:set ft=python sw=4 et:
